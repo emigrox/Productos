e7bb3e294039d9c6e2b28bf5b639cef9991ff49a
EmilianoAdelarBade <58085172+emigrox@users.noreply.github.com>
2022-04-09 18:16:45 -0300

    creacion de Clase modelo Producto con los servicios y controladores asociados para realizar operaciones http

---
+++ src/main/java/com/proyectoBanco/productorestproyect/ProductoRestProyectApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoBanco.productorestproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoRestProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoRestProyectApplication.class, args);
+	}
+
+}
